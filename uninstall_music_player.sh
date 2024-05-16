bc2d314c5d7c4e344751e7f546671fa1ba198a8b
DerSpletti <96838084+DerSpletti@users.noreply.github.com>
2024-05-16 15:15:49 +0200

    Update uninstall_music_player.sh

--- uninstall_music_player.sh
+++ uninstall_music_player.sh
@@ -3,11 +3,13 @@
 # Stoppen des Music Player Daemon
 sudo systemctl stop mpd.service
 
-# Deinstallation der Pakete
+# Deinstallation der installierten Pakete
 sudo apt-get remove --purge -y mpd mpc
 
-# Entfernen des Mountpunkts für den USB-Stick
+# Entfernen des Eintrags aus der /etc/fstab
 sudo sed -i '/\/media\/usb/d' /etc/fstab
+
+# Unmounten und Entfernen des Mountpunktes für den USB-Stick
 sudo umount /media/usb
 sudo rmdir /media/usb
 
@@ -17,11 +19,12 @@ sudo rm -f /usr/local/bin/play_music.sh
 # Entfernen des Crontab-Eintrags
 (crontab -l | grep -v '/usr/local/bin/play_music.sh') | crontab -
 
-# Wiederherstellen der Original-MPD-Konfiguration
-sudo sed -i '/music_directory/c\music_directory "/var/lib/mpd/music"' /etc/mpd.conf
+# Wiederherstellen der ursprünglichen MPD-Konfiguration, falls notwendig
+# Dies hängt von Ihrer ursprünglichen MPD-Konfiguration ab.
+# Zum Beispiel: sudo sed -i '/music_directory/c\music_directory "/var/lib/mpd/music"' /etc/mpd.conf
 
-# Optionale Bereinigung: Entfernen nicht mehr benötigter Pakete
+# Entfernen nicht mehr benötigter Abhängigkeiten
 sudo apt-get autoremove -y
 
-# Neustarten des Systems, um alle Änderungen wirksam zu machen (optional)
+# Optionaler Neustart oder Meldung, dass der Vorgang abgeschlossen ist
 echo "Deinstallation abgeschlossen. Es wird empfohlen, das System neu zu starten."
