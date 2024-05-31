615ccca61269d7c7ee7813dea65b2764cdd3bba2
DerSpletti <96838084+DerSpletti@users.noreply.github.com>
2024-05-31 12:38:18 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,26 @@ sudo apt update
 sudo apt install -y mpg123 autofs
 
 echo "Configuring autofs..."
+
+# Suchen Sie nach dem USB-Gerät
+USB_DEVICE=$(lsblk -o NAME,TRAN | grep 'usb' | awk '{print $1}' | head -n 1)
+
+# Prüfen, ob das Gerät existiert und ob es eine Partition gibt
+if lsblk | grep -q "${USB_DEVICE}1"; then
+  USB_DEVICE="${USB_DEVICE}1"
+fi
+
+# Bestätigung der Geräteerkennung
+echo "Der Stick wurde als /dev/$USB_DEVICE erkannt. Ist das richtig? (y/n)"
+read confirmation
+
+if [[ $confirmation != "y" ]]; then
+  echo "Installation abgebrochen."
+  exit 1
+fi
+
 echo "/media/usb /etc/auto.usb --timeout=10" | sudo tee -a /etc/auto.master > /dev/null
-echo "usb1 -fstype=vfat,rw,uid=pi,gid=pi :/dev/sda1" | sudo tee /etc/auto.usb > /dev/null
+echo "usb1 -fstype=vfat,rw,uid=pi,gid=pi :/dev/$USB_DEVICE" | sudo tee /etc/auto.usb > /dev/null
 sudo systemctl restart autofs
 
 echo "Setting up music player script..."
